f91ac73016fa6fdb77b5b16c46569d4b09fc0f46
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-29 11:47:23 +0200

    exercise video boilerplate

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
 
-const ExerciseVideos = () => {
-  return <div>ExerciseVideos</div>;
+const ExerciseVideos = ({ exerciseVideos, name }) => {
+  return (
+    <Box sx={{ marginTop: { lg: '200px', sx: '20px' } }} p='20px'>
+      <Typography variant='h3' mb='33px'>
+        Watch{' '}
+        <span style={{ color: '#ff2625' }} textTransform='capitalize'>
+          {name}
+        </span>{' '}
+        exercise videos
+      </Typography>
+      <Stack></Stack>
+    </Box>
+  );
 };
 
 export default ExerciseVideos;
