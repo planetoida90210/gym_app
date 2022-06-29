489640e2069c9d6d5237f897895d42c2abd467d2
planetoida90210 <galeriakoloru90210@gmail.com>
2022-06-29 12:55:59 +0200

    implement exercise videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return 'Loading...';
+
   return (
     <Box sx={{ marginTop: { lg: '200px', sx: '20px' } }} p='20px'>
       <Typography variant='h3' mb='33px'>
@@ -11,7 +13,32 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         </span>{' '}
         exercise videos
       </Typography>
-      <Stack></Stack>
+      <Stack
+        justifyContent='flex-start'
+        flexWrap='wrap'
+        alignItems='center'
+        sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0' } }}
+      >
+        {exerciseVideos?.slice(0, 3).map((item, index) => (
+          <a
+            key={index}
+            className='exercise-video'
+            href={`https://youtube.com/watch?v=${item.video.videoId}`}
+            target='_blank'
+            rel='noreferrer'
+          >
+            <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant='h5' color='#000'>
+                {item.video.title}
+              </Typography>
+              <Typography variant='h6' color='#000'>
+                {item.video.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
     </Box>
   );
 };
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -25,10 +25,10 @@ const ExerciseDetail = () => {
       setExerciseDetail(exerciseDetailData);
 
       const exerciseVideosData = await fetchData(
-        `${youtubeSearchUrl}/search?q=${exerciseDetailData.name}`,
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name}`,
         youtubeOptions
       );
-      setExerciseVideos(exerciseVideosData);
+      setExerciseVideos(exerciseVideosData.contents);
     };
     fetchExercisesData();
   }, [id]);
